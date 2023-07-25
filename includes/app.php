df6cfc7e384f5a076684014cb6a57b2a2ce45b24
3ptnDesarrollo <afuentesj1@mindef.mil.gt>
2023-07-24 23:06:33 -0600

    SE AGREGO VARIABLE DE ENTORNO PARA DEBUGUEAR

--- includes/app.php
+++ includes/app.php
@@ -8,6 +8,10 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
+ini_set('display_errors', $_ENV['DEBUG_MODE']);
+ini_set('display_startup_errors', $_ENV['DEBUG_MODE']);
+error_reporting(-$_ENV['DEBUG_MODE']);
+
 require 'funciones.php';
 require 'database.php';
 // Conectarnos a la base de datos
